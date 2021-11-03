eec1992f435732ba8f4e55471cc67b562ae7be18
Ameer <Amir.abh1996@gmail.com>
2021-11-02 21:58:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const HQ = 42;
+const feet =264;
+function distanceFromHqInBlocks(pickuplocation) {
+    console.log(Math.abs(HQ - pickuplocation))
+   return Math.abs (HQ - pickuplocation);
+}
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * feet;
+}
+function distanceTravelledInFeet(HQ, num){
+        return Math.abs((num-HQ)*feet);
+    
+}
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination)
+    } 
+    
+}
